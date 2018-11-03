947ed722f5ee7ec0bc2b290ca96a6cdf72d86461
Junian Triajianto <author@junian.net>
2018-11-03 13:03:49 +0700

    Add  nullable float converter

---
+++ src/ProductCalculator/Converters/NullableFloatConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ProductCalculator.Converters
+{
+    public class NullableFloatConverter : IValueConverter
+    {
+        public NullableFloatConverter()
+        {
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (float?)value;
+            return val.HasValue ? val.ToString() : string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float? result = null;
+            try
+            {
+                result = float.Parse(value.ToString());
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+            return result;
+        }
+    }
+}
