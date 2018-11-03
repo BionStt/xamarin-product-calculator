982857128771b787d46ef7f58281a21fb25b3041
Junian Triajianto <author@junian.net>
2018-11-04 06:48:42 +0700

    Add xam.plugins.settings

---
+++ src/ProductCalculator/Services/Settings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ProductCalculator.Services
+{
+    public static class Settings
+    {
+        private static ISettings AppSettings
+        {
+            get
+            {
+                return CrossSettings.Current;
+            }
+        }
+
+        public static int SelectedLanguageIndex
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedLanguageIndex), 0);
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedLanguageIndex), value);
+        }
+
+        public static string SelectedLanguageCode
+        {
+            get => AppSettings.GetValueOrDefault(nameof(SelectedLanguageCode), "en");
+            set => AppSettings.AddOrUpdateValue(nameof(SelectedLanguageCode), value);
+        }
+    }
+}
