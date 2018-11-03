d4f9757ae64b00d835b874a547ec4e01adf74b2a
Junian Triajianto <author@junian.net>
2018-11-03 13:02:37 +0700

    Add bool inversion converteres

---
+++ src/ProductCalculator/Converters/BoolInversionConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace ProductCalculator.Converters
+{
+    public class BoolInversionConverter : IValueConverter
+    {
+        public BoolInversionConverter()
+        {
+        }
+
+        #region IValueConverter implementation
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value == null) return null;
+            var val = (bool)value;
+            return !val;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
