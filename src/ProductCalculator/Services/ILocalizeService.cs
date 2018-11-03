e595234436401bbcd98a2a83cad6978469315a10
Junian Triajianto <author@junian.net>
2018-11-04 06:07:33 +0700

    Add localize implementation

--- src/ProductCalculator/Services/ILocalizeService.cs
+++ src/ProductCalculator/Services/ILocalizeService.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 
 namespace ProductCalculator.Services
 {
-    public interface ILocalize
+    public interface ILocalizeService
     {
         CultureInfo GetCurrentCultureInfo();
         void SetLocale(CultureInfo ci);
