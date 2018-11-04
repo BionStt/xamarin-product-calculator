5aa24e979bd94ada5569ee01f48d9dabdd1bce71
Junian Triajianto <author@junian.net>
2018-11-04 07:11:34 +0700

    Add translate extension

---
+++ src/ProductCalculator/Extensions/TranslateExtension.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using ProductCalculator.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProductCalculator.Extensions
+{
+    [ContentProperty("Text")]
+    public class TranslateExtension : IMarkupExtension
+    {
+        readonly CultureInfo ci;
+        const string ResourceId = "SMFive.Pretension.Calculator.Resources.AppResources";
+
+        private static readonly Lazy<ResourceManager> ResMgr = new Lazy<ResourceManager>(() => new ResourceManager(ResourceId
+                                                                                                                  , typeof(TranslateExtension).GetTypeInfo().Assembly));
+
+        public TranslateExtension()
+        {
+            if (Device.RuntimePlatform == Device.iOS || Device.RuntimePlatform == Device.Android)
+            {
+                ci = DependencyService.Get<ILocalizeService>().GetCurrentCultureInfo();
+                var langCode = Settings.SelectedLanguageCode;
+                ci = new CultureInfo(langCode);
+                Resources.AppResources.Culture = ci; // set the RESX for resource localization
+                DependencyService.Get<ILocalizeService>().SetLocale(ci); // set the Thread for locale-aware methods
+            }
+        }
+
+        public string Text { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Text == null)
+                return "";
+
+            var translation = ResMgr.Value.GetString(Text, ci);
+
+            if (translation == null)
+            {
+#if DEBUG
+                throw new ArgumentException(
+                    String.Format("Key '{0}' was not found in resources '{1}' for culture '{2}'.", Text, ResourceId, ci.Name),
+                    "Text");
+#else
+                translation = Text; // returns the key, which GETS DISPLAYED TO THE USER
+#endif
+            }
+            return translation;
+        }
+    }
+}
