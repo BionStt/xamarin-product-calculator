8324992226c810d8a466ab5b529f336018235a11
Junian Triajianto <author@junian.net>
2018-11-20 06:06:43 +0700

    Add crosspicker renderer

---
+++ src/ProductCalculator.Android/Renderers/CrossPickerRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics.Drawables;
+using ProductCalculator.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Picker), typeof(CrossPickerRenderer))]
+namespace ProductCalculator.Droid.Renderers
+{
+    public class CrossPickerRenderer : PickerRenderer
+    {
+
+        public CrossPickerRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+                return;
+
+            if (Element == null)
+                return;
+
+            this.Control.Background = new ColorDrawable(Element.BackgroundColor.ToAndroid());
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            if (this.Element == null)
+                return;
+
+            if (e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
+            {
+                this.Control.Background = new ColorDrawable(Element.BackgroundColor.ToAndroid());
+            }
+        }
+    }
+}
