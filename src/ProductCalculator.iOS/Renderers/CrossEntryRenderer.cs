4c7b0862eeab473b2e78b21bb3d378c2b7866d58
Junian Triajianto <author@junian.net>
2018-11-20 05:57:22 +0700

    Add crossentryrenderer

---
+++ src/ProductCalculator.iOS/Renderers/CrossEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ProductCalculator.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(CrossEntryRenderer))]
+namespace ProductCalculator.iOS.Renderers
+{
+    public class CrossEntryRenderer : EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+                return;
+
+            if (Element == null)
+                return;
+
+            this.Control.Layer.BorderWidth = 1.0f;
+            this.Control.Layer.BorderColor = UIColor.Clear.CGColor;
+        }
+
+    }
+}
