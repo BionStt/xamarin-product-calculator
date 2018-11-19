94aecc1e2934f9c91cf57dc674d00eef3e74f9ac
Junian Triajianto <author@junian.net>
2018-11-20 06:02:58 +0700

    Add crosseditor renderer

---
+++ src/ProductCalculator.Android/Renderers/CrossEditorRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.Content;
+using Android.Graphics.Drawables;
+using ProductCalculator.Droid.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(CrossEditorRenderer))]
+namespace ProductCalculator.Droid.Renderers
+{
+    public class CrossEditorRenderer : EditorRenderer
+    {
+        public CrossEditorRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+
+            if (Control == null)
+                return;
+
+            if (Element == null)
+                return;
+
+            this.Control.Background = new ColorDrawable(Element.BackgroundColor.ToAndroid());
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+            if (this.Element == null)
+                return;
+
+            if (e.PropertyName == VisualElement.BackgroundColorProperty.PropertyName)
+            {
+                this.Control.Background = new ColorDrawable(Element.BackgroundColor.ToAndroid());
+            }
+        }
+    }
+}
